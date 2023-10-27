884f3e4534c6e7a58717507376802d8a3d2effcc
tyuiu.bogdanovasnezhana@outlook.com <bogdanova.snezha09@gmail.com>
2023-10-28 00:13:54 +0500

    Task6

---
+++ Tyuiu.BogdanovaSA.Sprint3.Task6.V19.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BogdanovaSA.Sprint3.Task6.V19.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BogdanovaSA.Sprint3.Task6.V19.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7eea1474-ccef-4733-abf4-2fbf57b95f49")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.BogdanovaSA.Sprint3.Task6.V19/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BogdanovaSA.Sprint3.Task6.V19.Lib;
+
+namespace Tyuiu.BogdanovaSA.Sprint3.Task6.V19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнила: Богданова С. А.  | ИСПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("*Тема: Создание итогового решения по спринту                              *");
+            Console.WriteLine("*Задание #Task6                                                           *");
+            Console.WriteLine("*Вариант #19                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Написать программу, которая подсчитает кол-во делителей меньше 8         ");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+
+        
+            DataService service1 = new DataService();
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            int x = 10;
+            int y = 15;
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = service1.GetSumTheDivisors(x, y);
+
+            Console.WriteLine("По моим подсчётам " + res);
+
+
+            Console.ReadKey();
+        }
+    }
+    }
+}
